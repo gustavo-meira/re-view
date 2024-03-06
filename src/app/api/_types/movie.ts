9e627d79b323783c47ea2d3f6219747467303425
Gustavo Meira <gmeira.antonio@gmail.com>
2024-03-06 15:32:46 -0300

    feat(api): create a new type for MovieDetailsFromTMDB

--- src/app/api/_types/movie.ts
+++ src/app/api/_types/movie.ts
@@ -1,4 +1,4 @@
-export type Movie = {
+export type MovieFromTMDB = {
   adult: boolean;
   backdrop_path: string;
   genre_ids: number[];
@@ -13,4 +13,52 @@ export type Movie = {
   video: boolean;
   vote_average: number;
   vote_count: number;
+};
+
+export type MovieDetailsFromTMDB = {
+  adult: boolean;
+  backdrop_path: string;
+  budget: number;
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  belongs_to_collection: {
+    id: number;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  };
+  genres: Array<{
+    id: number;
+    name: string;
+  }>;
+  production_companies: Array<{
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }>;
+  production_countries: Array<{
+    iso_3166_1: string;
+    name: string;
+  }>;
+  spoken_languages: Array<{
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }>;
 };
